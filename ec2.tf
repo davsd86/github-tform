eaab32d538303d89ac4123f1f8ac5a0bb8a5f130
Dav Dhesi <sysopr60@ip-10-244-39-83.eu-west-1.compute.internal>
2020-10-06 06:25:00 +0000

    tform code amended

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-    access_key = "AKIAW7UAMQYSEOP3ZG7H"
-    secret_key = "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"
-    region = "eu-west-1"
-}
-
 resource "aws_instance" test1 {
    ami = "ami-0bb3fad3c0286ebd5"
    instance_type = "t2.micro"
